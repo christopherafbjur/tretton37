8e4520b6fc1987464b17cc843570fdfdff23c57c
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-07-05 14:21:23 +0200

    WIP: Added basic employee GET request to App useEffect hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import api from '../services/api';
 
 function App() {
+  useEffect(() => {
+    async function getEmployeesFn() {
+      let employees = null;
+      try {
+        employees = await api();
+        console.log(employees);
+      } catch (ex) {
+        console.log(ex);
+      }
+    }
+    getEmployeesFn();
+  }, []);
+
   return <h1>Hello Tretton37</h1>;
 }
 
