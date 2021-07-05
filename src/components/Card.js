fb9d8f1aae52e0069be6239056c3d9ce5a5c42dd
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-07-05 17:11:59 +0200

    Refactor card component to use css module styles. Added social icons

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import style from './Card.module.css';
 
 function Card({ employee }) {
-  const { name, office, imagePortraitUrl: imageUrl } = employee;
+  const { name, office, imagePortraitUrl: imageUrl, gitHub, linkedIn, twitter } = employee;
   return (
-    <div className="profile-card" style={{ backgroundColor: 'gray' }}>
-      <div
-        className="image"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-          backgroundSize: 'cover',
-          width: '100px',
-          height: '100px',
-        }}
-      />
-      <div className="details">
-        <h1 className="name">{name}</h1>
-        <ul className="social">
-          <li>LinkedIn</li>
-          <li>Github</li>
-          <li>Twitter</li>
+    <div className={style.card}>
+      <div className={style.image} style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className={style.details}>
+        <h1 className={style.name}>{name}</h1>
+        <ul className={style.social}>
+          <li>
+            <a target="_blank" rel="noreferrer" href={`https://linkedin.com/${linkedIn}`}>
+              <FaLinkedin />
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noreferrer" href={`https://github.com/${gitHub}`}>
+              <FaGithub />
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noreferrer" href={`https://twitter.com/${twitter}`}>
+              <FaTwitter />
+            </a>
+          </li>
         </ul>
       </div>
-      <h1 className="office">Office: {office}</h1>
+      <h1 className={style.location}>Office: {office}</h1>
     </div>
   );
 }
