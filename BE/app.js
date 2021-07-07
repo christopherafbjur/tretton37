5ebc3b4fe792f3c7261e3435efad4c6bc8d06f67
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-07-07 12:13:00 +0200

    BE: Installed cors

--- BE/app.js
+++ BE/app.js
@@ -1,8 +1,10 @@
 const employeesRoute = require('./routes/employees');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 3001;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/employees', employeesRoute);
