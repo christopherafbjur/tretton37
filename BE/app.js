0dddd3743db87c048d5ea876f9b604fc3f653ad0
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-07-07 09:36:49 +0200

    Setup simple express server with employees route

---
+++ BE/app.js
@@ -0,0 +1,12 @@
+const employeesRoute = require('./routes/employees');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use('/employees', employeesRoute);
+app.get('/', function (req, res) {
+  res.send('Im alive');
+});
+app.listen(port, () => console.log(`Running server on port ${port}`));
