8b7cdae57e78f7fd91dab7a9e38ff87b6bcbe51b
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-07-07 11:47:14 +0200

    BE: Added route params (bad ones)

--- BE/routes/employees.js
+++ BE/routes/employees.js
@@ -3,7 +3,22 @@ const router = express.Router();
 const db = require('../services/database');
 
 router.get('/', async function (req, res) {
-  const query = `SELECT * FROM users ORDER BY name asc`;
+  const options = {
+    query: req.query.query || '',
+    orderBy: req.query.orderBy || 'name',
+    direction: req.query.direction || 'ASC',
+    limit: req.query.limit || null,
+    offset: req.query.offset || null,
+  };
+
+  //! Note, I'm aware that this approach opens up for SQL injection but don't have time to migrate to other postgres lib
+  const query = `
+    SELECT * FROM users 
+    WHERE ${options.orderBy} ILIKE '%${options.query}%'
+    ORDER BY ${options.orderBy} ${options.direction}
+    LIMIT ${options.limit}
+    OFFSET ${options.offset}
+  `;
 
   db.query(query, function (err, result) {
     if (err) throw err;
