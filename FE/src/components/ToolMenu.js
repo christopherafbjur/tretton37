a14d93c22a8140cc2dbdc8ae7eff13f7c5f923ea
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-07-07 17:35:36 +0200

    CSS modifications to ToolMenu component

--- FE/src/components/ToolMenu.js
+++ FE/src/components/ToolMenu.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import style from './ToolMenu.module.css';
 
 function ToolMenu({ onQueryChange, onOfficeChange, onSortChange, onSortDirectionChange }) {
   return (
     <>
-      <div style={{ backgroundColor: 'orange' }}>
-        <h4>Filter</h4>
-        <input type="text" onChange={onQueryChange} placeholder="Name" />
-        <select id="office" onChange={onOfficeChange} name="office">
-          <option value="">Office</option>
-          <option value="lund">Lund</option>
-          <option value="helsingborg">Helsingborg</option>
-          <option value="stockholm">Stockholm</option>
-          <option value="borlänge">Borlänge</option>
-          <option value="ljubljana">Ljubljana</option>
-        </select>
-      </div>
-      <div style={{ backgroundColor: 'orange' }}>
-        <h4>Sort</h4>
-        <select id="sortBy" onChange={onSortChange} name="sortBy">
-          <option value="">Sort by</option>
-          <option value="office">Office</option>
-          <option value="name">Name</option>
-        </select>
-        <h4>Direction</h4>
-        <select id="direction" onChange={onSortDirectionChange} name="direction">
-          <option value="">Direction</option>
-          <option value="ASC">Ascending</option>
-          <option value="DESC">Descending</option>
-        </select>
+      <div className={style.mainWrapper}>
+        <div className={style.toolWrapper}>
+          <div className={style.filterWrapper}>
+            <h4>Filter</h4>
+            <input type="text" onChange={onQueryChange} placeholder="Name" />
+            <select id="office" onChange={onOfficeChange} name="office">
+              <option value="">Office</option>
+              <option value="lund">Lund</option>
+              <option value="helsingborg">Helsingborg</option>
+              <option value="stockholm">Stockholm</option>
+              <option value="borlänge">Borlänge</option>
+              <option value="ljubljana">Ljubljana</option>
+            </select>
+          </div>
+          <div className={style.sortWrapper}>
+            <h4>Sort</h4>
+            <select id="sortBy" onChange={onSortChange} name="sortBy">
+              <option value="">Sort by</option>
+              <option value="office">Office</option>
+              <option value="name">Name</option>
+            </select>
+            <h4>Direction</h4>
+            <select id="direction" onChange={onSortDirectionChange} name="direction">
+              <option value="">Direction</option>
+              <option value="ASC">Ascending</option>
+              <option value="DESC">Descending</option>
+            </select>
+          </div>
+        </div>
       </div>
     </>
   );
