27357c845387341122bd13c1aed7f5be51dea0d6
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-07-07 09:43:41 +0200

    BE: Setup postgres and integrate to employees route

--- BE/routes/employees.js
+++ BE/routes/employees.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../services/database');
 
 router.get('/', async function (req, res) {
-  res.send('test');
+  const query = `SELECT * FROM users ORDER BY name asc`;
+
+  db.query(query, function (err, result) {
+    if (err) throw err;
+    res.status(200).send(result.rows);
+  });
 });
 
 module.exports = router;
